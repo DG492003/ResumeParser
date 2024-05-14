007a0eaf821558de3a3b98f1fe230d31a3bcde8a
DIVYANSH GUPTA <113435632+DG492003@users.noreply.github.com>
2024-05-14 23:11:13 +0530

    Update settings.py

--- multiupload/settings.py
+++ multiupload/settings.py
@@ -121,9 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myapp/static'),  # This assumes you have a 'static' directory in your project root
 ]
 
 
