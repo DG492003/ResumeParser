7c49b2cc76d4a8fe632d03e8052d93d776ec5528
DIVYANSH GUPTA <113435632+DG492003@users.noreply.github.com>
2024-05-14 21:49:49 +0530

    Update settings.py

--- multiupload/settings.py
+++ multiupload/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'multiupload.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
